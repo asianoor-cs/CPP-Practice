0df699b836dd20e167b73e9a92f7f24c4e926070
Asia Noor <asianoor984@gmail.com>
2026-07-05 03:05:06 -0700

    Create my_information.cpp

---
+++ my_information.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Name: Asia Noor" << endl;
+    cout << "Degree: BS Computer Science" << endl;
+    cout << "University: Rawalpindi Women University" << endl;
+
+    return 0;
+}
