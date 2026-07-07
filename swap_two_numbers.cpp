204da8f71396395fed4b22bdb381604fd206dfd5
Asia Noor <asianoor984@gmail.com>
2026-07-07 02:59:58 -0700

    Create swap_two_numbers.cpp

---
+++ swap_two_numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, temp;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    cout << "After swapping:" << endl;
+    cout << "First Number = " << num1 << endl;
+    cout << "Second Number = " << num2 << endl;
+
+    return 0;
+}
